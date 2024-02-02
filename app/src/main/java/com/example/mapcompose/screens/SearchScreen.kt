c8c99fcd169508a7514c8c935725d351487601e9
DanielBuenoNavarroo <dabue410@alu.centroafuera.es>
2024-02-02 17:13:17 +0100

    Añadida funcionalidad a SearchScreen.kt

--- app/src/main/java/com/example/mapcompose/screens/SearchScreen.kt
+++ app/src/main/java/com/example/mapcompose/screens/SearchScreen.kt
@@ -1,19 +1,37 @@
 package com.example.mapcompose.screens
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.mapcompose.Componentes
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -39,11 +57,45 @@ fun SearchScreen(navController: NavController) {
 
 @Composable
 fun Body() {
-    Column {
-        Text(text = "HOlA")
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Siguiente")
+    var busqueda by rememberSaveable { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Componentes.MarginVertical(margin = 64)
+        TextField(
+            value = busqueda,
+            onValueChange = { busqueda = it },
+            label = { Text("Ingrese texto") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+
+        Button(
+            onClick = {
+                search(context, busqueda)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = "Search")
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null)
         }
     }
+}
+
+fun search(context: android.content.Context, query: String) {
+    val intent = Intent(Intent.ACTION_VIEW, android.net.Uri.parse("https://www.google.com/search?q=$query"))
+    context.startActivity(intent)
 }
 
